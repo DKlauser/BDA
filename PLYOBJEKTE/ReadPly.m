a8763d7df6977f9b24ff1b53563d2c1e5ec0a31c
DKlauser <daniel.klauser@stud.hslu.ch>
2017-04-11 10:13:11 +0200

    Funktioniert diese Lösungen

--- PLYOBJEKTE/ReadPly.m
+++ PLYOBJEKTE/ReadPly.m
@@ -1,42 +1,74 @@
-P1 = imread('multi_view_pipe_joints_cam_0_01.png')
-P2 = imread('multi_view_pipe_joints_cam_1_01.png')
-P3 = imread('multi_view_pipe_joints_cam_2_01.png')
-P4 = imread('multi_view_pipe_joints_cam_3_01.png')
+% P1 = imread('calib_3_1.bmp');
+% P2 = imread('calib_3_2.bmp');
+% P3 = imread('calib_3_3.bmp');
+% P4 = imread('calib_3_4.bmp');
+% P5 = imread('calib_3_5.bmp');
+% P6 = imread('calib_3_0.bmp');
+% P6 = imread('calib_1_6.bmp');
+% 
+% P1 = (P1-20)*2;
+% P2 = (P2-20)*2;
+% P3 = (P3-20)*2;
+% P4= (P4-20)*2;
+% P5 = (P5-20)*2;
+% P6 = (P6-20)*2;
+% 
+% % figure(1)
+% % hold on
+% % subplot(2,2,1)
+% % imshow(P1)
+% % subplot(2,2,2)
+% % imshow(P2)
+% % subplot(2,2,3)
+% % imshow(P3)
+% % subplot(2,2,4)
+% % imshow(P1)
+% % hold off
+% alpha = 0.2;
+% 
+% H =[0 0 0; 0 1 0; 0 0 0]+0.2*[0 -1 0; -1 4 -1; 0 -1 0];
+% %fspecial('laplacian',alpha);
+% 
+% Lap0 = imfilter(P6,H,'replicate');
+% Lap1 = imfilter(P1,H,'replicate');
+% Lap2 = imfilter(P2,H,'replicate');
+% Lap3 = imfilter(P3,H,'replicate');
+% Lap4 = imfilter(P4,H,'replicate');
+% Lap5 = imfilter(P5,H,'replicate');
+% 
+% 
+% figure(2)
+% hold on
+% subplot(2,2,1)
+% imshow(Lap0)
+% subplot(2,2,2)
+% imshow(Lap1)
+% subplot(2,2,3)
+% imshow(Lap2)
+% subplot(2,2,4)
+% imshow(Lap3)
+% 
+% 
+%  imwrite(Lap0,'myclown1.png')
+%  imwrite(Lap1,'myclown2.png')
+%  imwrite(Lap2,'myclown3.png')
+%  imwrite(Lap3,'myclown4.png')
+%   imwrite(Lap4,'myclown5.png')
+%  imwrite(Lap5,'myclown6.png')
+%%
+level = 0.4;
+s = 3;
 
-figure(1)
-hold on
-subplot(2,2,1)
-imshow(P1)
-subplot(2,2,2)
-imshow(P2)
-subplot(2,2,3)
-imshow(P3)
-subplot(2,2,4)
-imshow(P1)
-hold off
-
-
-H = fspecial('motion',5,12)
-
-Blur1 = imfilter(P1,H,'replicate')
-Blur2 = imfilter(P2,H,'replicate')
-Blur3 = imfilter(P3,H,'replicate')
-Blur4 = imfilter(P4,H,'replicate')
-
-figure(2)
-hold on
-subplot(2,2,1)
-imshow(Blur1)
-subplot(2,2,2)
-imshow(Blur2)
-subplot(2,2,3)
-imshow(Blur3)
-subplot(2,2,4)
-imshow(Blur4)
-
-
-imwrite(Blur1,'myclown1.png')
-imwrite(Blur2,'myclown2.png')
-imwrite(Blur3,'myclown3.png')
-imwrite(Blur4,'myclown4.png')
-
+P32 = imread('scheiben_eq_0.bmp');
+imhist(P32);
+for c = 0:s
+    s = strcat('scheiben_',int2str(c),'.bmp');
+    %s = strcat('Schachbrett.bmp');
+    P1 = imread(s);
+    bw = ((P1-17)*3);
+    cat = strcat('scheiben_eq_',int2str(c),'.bmp');
+    imwrite(bw,cat);
+end
+%%
+cloud = pcread('Punktewolke.ply')
+pcshow(cloud)
